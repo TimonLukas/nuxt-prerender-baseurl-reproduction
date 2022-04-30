cf636dc97cc3f9d38c464fbbcacf54335b00257b
Timon Lukas <timon.lukas3@gmail.com>
2022-04-30 19:09:40 +0200

    Add reproduction stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,24 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
   target: "static",
   ssr: false,
+  hooks: {
+    "nitro:config"(config) {
+      if (process.env.WORKAROUND !== "true") {
+        return
+      }
+
+      if (
+        !config.prerender?.crawlLinks ||
+        !Array.isArray(config.prerender?.routes)
+      ) {
+        return
+      }
+
+      const routes = config.prerender!.routes as string[]
+
+      config.prerender!.routes = routes.map((route) =>
+        route.replace("/", config.baseURL!)
+      )
+    }
+  }
 })
